7a1306ecb3aaeba5c28b11dff495353e5653f580
Administrator <lshaohua5@163.com>
2018-01-29 20:59:47 +0800

    加密

--- dist/js/order.js
+++ dist/js/order.js
@@ -3,7 +3,43 @@ $(function() {
     $('header').on('touchend', '.iconfont', function() {
         window.history.back();
     })
-
+    var ts = new Date().getTime();
+    var time = new Date().getTime();
+    console.log(time);
+    time = 'ts=' + time;
+    var pr = 'productId=1';
+    var arr = [pr, time];
+    arr = arr.sort();
+    var stringA = arr.join('&');
+    var stringSignTemp = stringA + '&key=innergluta@2018';
+    var sign = hex_md5(stringSignTemp).toUpperCase();
+    console.log(sign);
+    //getProduct(ts, sign);
+    //获取商品详情
+    function getProduct(ts, sign) {
+        $.ajax({
+            url: '/api/product.php',
+            type: 'get',
+            dataType: "JSON",
+            async: false,
+            data: {
+                productId: 1,
+                device: '',
+                ts: ts,
+                sign: sign
+            },
+            success: function(data) {
+                console.log(data)
+                $('.shop-car .goods-price').text('￥' + data.data.price);
+                $('.money .total-price').text('￥' + data.data.price);
+                $('.money .price').text('￥' + data.data.price);
+                $('footer .total-price').text('￥' + data.data.price);
+            },
+            error: function() {
+                console.log('商品详情API获取失败')
+            }
+        })
+    }
 
     //判断用户页面来源
     var url = window.location.href.split('?')[1];
@@ -36,22 +72,24 @@ $(function() {
     }
     //商品加 减
     var num = 1; //初始化商品数量
-    function shop(flag) {
+
+
+    function shop(flag, price) {
         if (flag > 0) {
             num++
         } else if (num >= 2) {
             num--
         }
         if (num == 1) {
-            $('.shop-num .price').text('￥' + 1288.00);
+            $('.shop-num .price').text('￥' + price);
             $('.discount .discount-info').text('无优惠');
-            $('.price .total-price').text('￥' + 1288.00);
-            $('footer .total-price').text('￥' + 1288.00);
+            $('.price .total-price').text('￥' + price);
+            $('footer .total-price').text('￥' + price);
         } else {
-            $('.shop-num .price').text('￥' + (num * 1288).toFixed(2));
-            $('.discount .discount-info').text('￥' + (num * 1288 * 0.2).toFixed(2));
-            $('.price .total-price').text('￥' + (num * 1288 * 0.8).toFixed(2));
-            $('footer .total-price').text('￥' + (num * 1288 * 0.8).toFixed(2));
+            $('.shop-num .price').text('￥' + (num * price).toFixed(2));
+            $('.discount .discount-info').text('￥' + (num * price * 0.2).toFixed(2));
+            $('.price .total-price').text('￥' + (num * price * 0.8).toFixed(2));
+            $('footer .total-price').text('￥' + (num * price * 0.8).toFixed(2));
         }
         $('.goods-num').text(num);
         $('.shop-num .num').text(num);
@@ -59,7 +97,10 @@ $(function() {
 
     $('.shopping').on('touchend', '.iconfont', function() {
         var flag = $(this).attr('data-num');
-        shop(flag);
+        var price = $('.shop-car .goods-price').text(); //商品价格
+        price = price.substring(1);
+        console.log(price);
+        shop(flag, price);
     })
 
 
@@ -67,7 +108,26 @@ $(function() {
         if (url == undefined) {
             layer.msg('请填写收货人地址')
         } else {
-            create_order();
+            var price = $('.shop-car .goods-price').text(); //商品价格
+            price = price.substring(1);
+            var userInfo = sessionStorage.getItem('userInfo');
+            userInfo = JSON.parse(userInfo);
+            var ts_create = new Date().getTime();
+            var name = userInfo.name;
+            var cell = userInfo.cell;
+            var address = userInfo.pro + userInfo.address;
+            var num = $('.shop-num .num').text();
+            var num_sign = 'num=' + num;
+            var time_sign = time;
+            var price_sign = 'price=' + price;
+            var name_sign = 'name=' + name;
+            var cell_sign = 'cell=' + cell;
+            var address_sign = 'address=' + address;
+            var arr1 = [price_sign, name_sign, cell_sign, address_sign, num_sign, time_sign];
+            arr1 = arr1.sort();
+            var str1 = arr1.join('&') + '&key=innergluta@2018';
+            sign1 = hex_md5(str1).toUpperCase();
+            create_order(price, name, cell, address, num, ts_create, sign1);
         }
     })
 
@@ -76,24 +136,6 @@ $(function() {
         $('.shade').show();
         $('.shade_wrapper').show();
     }
-
-    //刷新頁面
-    function reurl() {
-        var curUrl = location.href;
-        var times = curUrl.split("#");
-        if (times[1] != 1) {
-            curUrl += "#1";
-            self.location.href = curUrl;
-        }
-    }
-    //判断安卓手机  刷新页面
-    // var u = navigator.userAgent;
-    // var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
-    // var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-    // if (isAndroid) {
-    //     alert(2222)
-    //     reurl()
-    // }
 
     //判斷瀏覽器訪問的頁面
     if (sessionStorage.getItem('payInfo')) {
@@ -105,7 +147,14 @@ $(function() {
         var payInfo = sessionStorage.getItem('payInfo');
         payInfo = JSON.parse(payInfo);
         var orderId = payInfo.orderId;
-        orderStatus(orderId);
+        var orderId_status = 'orderId=' + orderId;
+        var ts_status = new Date().getTime();
+        var time_status = 'ts=' + ts_status;
+        var arr_status = [orderId_s, time_status];
+        arr_status = arr_status.sort();
+        var str_status = arr_status.join('&') + '&key=innergluta@2018';
+        sign_status = hex_md5(str_status).toUpperCase();
+        orderStatus(orderId, ts_status, sign_status);
     })
 
     //点击未支付支付
@@ -114,32 +163,45 @@ $(function() {
         var payInfo = sessionStorage.getItem('payInfo');
         payInfo = JSON.parse(payInfo);
         var orderId = payInfo.orderId;
-        orderStatus(orderId);
+        var orderId_status = 'orderId=' + orderId;
+        var ts_status = new Date().getTime();
+        var time_status = 'ts=' + ts_status;
+        var arr_status = [orderId_s, time_status];
+        arr_status = arr_status.sort();
+        var str_status = arr_status.join('&') + '&key=innergluta@2018';
+        sign_status = hex_md5(str_status).toUpperCase();
+        orderStatus(orderId, ts_status, sign_status);
     })
 
     //确认下单
-    function create_order() {
+    function create_order(price, name, cell, address, num, ts, sign) {
         $.ajax({
             url: '/api/create_order.php',
             type: 'post',
             async: false,
             dataType: "JSON",
             data: {
-                productId: '',
-                price: 1288,
+                productId: 1,
+                price: price,
                 name: name,
                 cell: cell,
-                address: pro + address,
+                address: address,
                 num: num,
                 remark: '',
                 device: '',
-                sign: '',
-                sign: ''
+                ts: ts,
+                sign: sign
             },
             success: function(data) {
                 window.orderId = data.data.orderId;
-                console.log(orderId);
-                getUrl(orderId);
+                var orderId_s = 'orderId=' + orderId;
+                var ts_s = new Date().getTime();
+                var time_s = 'ts=' + ts_s;
+                var arr_s = [orderId_s, time_s];
+                arr_s = arr_s.sort();
+                var str_s = arr_s.join('&') + '&key=innergluta@2018';
+                sign_s = hex_md5(str_s).toUpperCase();
+                getUrl(orderId, ts_s, sign_s);
             },
             error: function() {
                 console.log('下单api获取失败')
@@ -147,14 +209,16 @@ $(function() {
         })
     }
     //获取支付url
-    function getUrl(orderId) {
+    function getUrl(orderId, ts, sign) {
         $.ajax({
             url: '/api/prepay.php',
             type: 'post',
             dataType: "JSON",
             async: false,
             data: {
-                orderId: orderId
+                orderId: orderId,
+                ts: ts,
+                sign: sign
             },
             success: function(data) {
                 var mweb_url = data.data.mweb_url;
@@ -173,7 +237,7 @@ $(function() {
         })
     }
     //查询订单状态
-    function orderStatus(orderId) {
+    function orderStatus(orderId, ts, sign) {
         $.ajax({
             url: '/api/query_order.php',
             type: 'get',
@@ -181,7 +245,8 @@ $(function() {
             dataType: "JSON",
             data: {
                 orderId: orderId,
-                sign: ''
+                ts: ts,
+                sign: sign
             },
             success: function(data) {
                 var orderStatu = data.data.orderStatus;
